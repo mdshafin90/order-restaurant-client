6eccd2159b067af1191e02f47320ea0b27e8f42b
shafin bisswas <mdnazmussakibshafin@gmail.com>
2023-06-18 02:11:21 +0600

    bootstrap project created successfully

---
+++ src/Layouts/Main.jsx
@@ -0,0 +1,10 @@
+
+const Main = () => {
+    return (
+        <div>
+            <h1 className="text-center display-1 text-primary">This is Main Component</h1>
+        </div>
+    );
+};
+
+export default Main;
